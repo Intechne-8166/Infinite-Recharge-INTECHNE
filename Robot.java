bacfec9561128de0984b1ea171efd960c9844844
Zach Battal <60168662+Ridde999@users.noreply.github.com>
2020-01-28 20:58:24 +0300

    shooter included final version
    shooter ve intake ekledim onaylandı

--- Robot.java
+++ Robot.java
@@ -17,7 +17,7 @@ public class Robot extends TimedRobot {
   private PWMVictorSPX left = new PWMVictorSPX(0);     //A fix might be to import PWMVictorSPX's and use them instead of WPI_VictorSPX's  /* Old Code private WPI_VictorSPX leftFront = new WPI_VictorSPX(0)
   private PWMVictorSPX right = new PWMVictorSPX(1); 
   
-  private final PWMVictorSPX m_shooterMotor1 = new PWMVictorSPX(2); //2nd old code SpeedController leftFront = new PWMVictorSPX
+  private final PWMVictorSPX shooterMotor1 = new PWMVictorSPX(2); //2nd old code SpeedController leftFront = new PWMVictorSPX
 
   private DifferentialDrive drive = new DifferentialDrive(left, right);
 
@@ -48,23 +48,95 @@ public class Robot extends TimedRobot {
 
     drive.arcadeDrive(power, turn);
     
-    if(m_stick.getRawButton(4))
+    if(Joystick.get(4))
     {
       //Shoot out
-      m_shooterMotor1.set(1);
-    } else
-    {
-      m_shooterMotor1.set(0);
+      shooterMotor1.set(1);
     }
 
-    if(m_stick.getRawButton(3))
+    if(Joystick.getRawButton(3))
     {
       //Take in
-      m_shooterMotor1.set(-1);
-    } else
+      shooterMotor1.set(-1);
+    } 
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+
+public class Robot extends TimedRobot {
+  private PWMVictorSPX left = new PWMVictorSPX(0);     //A fix might be to import PWMVictorSPX's and use them instead of WPI_VictorSPX's  /* Old Code private WPI_VictorSPX leftFront = new WPI_VictorSPX(0)
+  private PWMVictorSPX right = new PWMVictorSPX(1);    //2nd old code SpeedController leftFront = new PWMVictorSPX
+
+  private PWMVictorSPX shooterMotor1 = new PWMVictorSPX(2);
+
+  private DifferentialDrive drive = new DifferentialDrive(left, right);
+
+  private Joystick joystick1 = new Joystick(0);
+
+  @Override
+  public void robotInit() {
+    //If joystick degree is lower then 0.05 it doesnt move 
+    //drive.setDeadband(0.05);
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    double power = -joystick1.getRawAxis(1);
+    double turn = joystick1.getRawAxis(4);
+
+    drive.arcadeDrive(power, turn);
+
+    if(joystick1.getRawButtonPressed(4))
     {
-      m_shooterMotor1.set(0);
+      shooterMotor1.set(1);
     }
+    else if(joystick1.getRawButtonReleased(4))
+    {
+      shooterMotor1.set(0);
+    }
+
+    if(joystick1.getRawButtonPressed(3))
+    {
+      shooterMotor1.set(-1);
+    }
+    else if(joystick1.getRawButtonReleased(3))
+    {
+      shooterMotor1.set(0);
+    }
+
   }
 
   @Override
